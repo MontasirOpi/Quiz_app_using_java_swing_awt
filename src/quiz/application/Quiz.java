04c42a95a94124a0e6d585d9638fb2bc1640736d
montasir opi <montasiropi@gmail.com>
2024-06-12 00:23:42 +0600

    first commit

---
+++ src/quiz/application/Quiz.java
@@ -0,0 +1,313 @@
+package quiz.application;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Quiz extends JFrame implements ActionListener {
+    
+    String questions[][] = new String[10][5];
+    String answers[][] = new String[10][2];
+    String useranswers[][] = new String[10][1];
+    JLabel qno, question;
+    JRadioButton opt1, opt2, opt3, opt4;
+    ButtonGroup groupoptions;
+    JButton next, submit, lifeline;
+    
+    public static int timer = 15;
+    public static int ans_given = 0;
+    public static int count = 0;
+    public static int score = 0;
+    
+    String name;
+    
+    Quiz(String name) {
+        this.name = name;
+        setBounds(50, 0, 1440, 850);
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+        
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/bubt_quiz.jpeg"));
+        JLabel image = new JLabel(i1);
+        image.setBounds(0, 0, 1440, 392);
+        add(image);
+        
+        qno = new JLabel();
+        qno.setBounds(100, 450, 50, 30);
+        qno.setFont(new Font("Tahoma", Font.PLAIN, 24));
+        add(qno);
+        
+        question = new JLabel();
+        question.setBounds(150, 450, 900, 30);
+        question.setFont(new Font("Tahoma", Font.PLAIN, 24));
+        add(question);
+        
+        questions[0][0] = "When was BUBT founded?";
+        questions[0][1] = "1999";
+        questions[0][2] = "2003";
+        questions[0][3] = "2004";
+        questions[0][4] = "2001";
+
+        questions[1][0] = "What type of university is BUBT?";
+        questions[1][1] = "Private";
+        questions[1][2] = "Public";
+        questions[1][3] = "International";
+        questions[1][4] = "Community";
+
+        questions[2][0] = "In which country is BUBT located?";
+        questions[2][1] = "Mirupr";
+        questions[2][2] = "Uttara";
+        questions[2][3] = "Bashundhara";
+        questions[2][4] = "Gulshan";
+
+        questions[3][0] = "Who is the Vice Chancellor of BUBT?";
+        questions[3][1] = " Professor Dr. Abdul Karim";
+        questions[3][2] = "Professor Dr. Ayesha Rahman";
+        questions[3][3] = "Professor Dr. Muhammed Fayyaz Khan";
+        questions[3][4] = "Professor Dr. Sajid Ahmed";
+
+        questions[4][0] = "How many faculties does BUBT have for undergraduate programs?";
+        questions[4][1] = "1";
+        questions[4][2] = "4";
+        questions[4][3] = "3";
+        questions[4][4] = "5";
+
+        questions[5][0] = "What is the name of the BUBT student portal?";
+        questions[5][1] = "PortalPlus";
+        questions[5][2] = "Annex";
+        questions[5][3] = "StudentLink";
+        questions[5][4] = "EduHub";
+
+        questions[6][0] = "Which of the following is a key focus area for BUBT's academic programs?";
+        questions[6][1] = "Engineering and Technology";
+        questions[6][2] = "Arts and Humanities";
+        questions[6][3] = "Medical Sciences";
+        questions[6][4] = "Agricultural Sciences";
+
+        questions[7][0] = "BUBT offers programs at which levels of education?";
+        questions[7][1] = "Undergraduate only";
+        questions[7][2] = "Both undergraduate and graduate";
+        questions[7][3] = "Graduate only";
+        questions[7][4] = "Doctoral only";
+
+        questions[8][0] = "BUBT is accredited by which organization?";
+        questions[8][1] = "University Grants Commission (UGC) of Bangladesh";
+        questions[8][2] = "Bangladesh Medical and Dental Council";
+        questions[8][3] = "Bangladesh Bar Council";
+        questions[8][4] = "National University";
+
+        questions[9][0] = "Which of the following is a notable feature of BUBT's campus?";
+        questions[9][1] = "Extensive library facilities";
+        questions[9][2] = "Located in a rural area";
+        questions[9][3] = "No internet access";
+        questions[9][4] = "Only offers online courses";
+        
+        answers[0][1] = "2003";
+        answers[1][1] = "Private";
+        answers[2][1] = "Mirupr";
+        answers[3][1] = "Professor Dr. Muhammed Fayyaz Khan";
+        answers[4][1] = "5";
+        answers[5][1] = "Annex";
+        answers[6][1] = "Engineering and Technology";
+        answers[7][1] = "Both undergraduate and graduate";
+        answers[8][1] = "University Grants Commission (UGC) of Bangladesh";
+        answers[9][1] = "Extensive library facilities";
+        
+        opt1 = new JRadioButton();
+        opt1.setBounds(170, 520, 700, 30);
+        opt1.setBackground(Color.WHITE);
+        opt1.setFont(new Font("Dialog", Font.PLAIN, 20));
+        add(opt1);
+        
+        opt2 = new JRadioButton();
+        opt2.setBounds(170, 560, 700, 30);
+        opt2.setBackground(Color.WHITE);
+        opt2.setFont(new Font("Dialog", Font.PLAIN, 20));
+        add(opt2);
+        
+        opt3 = new JRadioButton();
+        opt3.setBounds(170, 600, 700, 30);
+        opt3.setBackground(Color.WHITE);
+        opt3.setFont(new Font("Dialog", Font.PLAIN, 20));
+        add(opt3);
+        
+        opt4 = new JRadioButton();
+        opt4.setBounds(170, 640, 700, 30);
+        opt4.setBackground(Color.WHITE);
+        opt4.setFont(new Font("Dialog", Font.PLAIN, 20));
+        add(opt4);
+        
+        groupoptions = new ButtonGroup();
+        groupoptions.add(opt1);
+        groupoptions.add(opt2);
+        groupoptions.add(opt3);
+        groupoptions.add(opt4);
+        
+        next = new JButton("Next");
+        next.setBounds(1100, 550, 200, 40);
+        next.setFont(new Font("Tahoma", Font.PLAIN, 22));
+        next.setBackground(new Color(30, 144, 255));
+        next.setForeground(Color.WHITE);
+        next.addActionListener(this);
+        add(next);
+        
+        lifeline = new JButton("50-50 Lifeline");
+        lifeline.setBounds(1100, 630, 200, 40);
+        lifeline.setFont(new Font("Tahoma", Font.PLAIN, 22));
+        lifeline.setBackground(new Color(30, 144, 255));
+        lifeline.setForeground(Color.WHITE);
+        lifeline.addActionListener(this);
+        add(lifeline);
+        
+        submit = new JButton("Submit");
+        submit.setBounds(1100, 710, 200, 40);
+        submit.setFont(new Font("Tahoma", Font.PLAIN, 22));
+        submit.setBackground(new Color(30, 144, 255));
+        submit.setForeground(Color.WHITE);
+        submit.addActionListener(this);
+        submit.setEnabled(false);
+        add(submit);
+        
+        start(count);
+        
+        setVisible(true);
+    }
+    
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == next) {
+            repaint();
+            opt1.setEnabled(true);
+            opt2.setEnabled(true);
+            opt3.setEnabled(true);
+            opt4.setEnabled(true);
+            
+            ans_given = 1;
+            if (groupoptions.getSelection() == null) {
+               useranswers[count][0] = "";
+            } else {
+                useranswers[count][0] = groupoptions.getSelection().getActionCommand();
+            }
+            
+            if (count == 8) {
+                next.setEnabled(false);
+                submit.setEnabled(true);
+            }
+            
+            count++;
+            start(count);
+        } else if (ae.getSource() == lifeline) {
+            if (count == 2 || count == 4 || count == 6 || count == 8 || count == 9) {
+                opt2.setEnabled(false);
+                opt3.setEnabled(false);
+            } else {
+                opt1.setEnabled(false);
+                opt4.setEnabled(false);
+            }
+            lifeline.setEnabled(false);
+        } else if (ae.getSource() == submit) {
+            ans_given = 1;
+            if (groupoptions.getSelection() == null) {
+                useranswers[count][0] = "";
+            } else {
+                useranswers[count][0] = groupoptions.getSelection().getActionCommand();
+            }
+
+            for (int i = 0; i < useranswers.length; i++) {
+                if (useranswers[i][0].equals(answers[i][1])) {
+                    score += 10;
+                } else {
+                    score += 0;
+                }
+            }
+            setVisible(false);
+            new Score(name, score);
+        }
+    }
+    
+    public void paint(Graphics g) {
+        super.paint(g);
+        
+        String time = "Time left - " + timer + " seconds"; // 15
+        g.setColor(Color.RED);
+        g.setFont(new Font("Tahoma", Font.BOLD, 25));
+        
+        if (timer > 0) { 
+            g.drawString(time, 1100, 500);
+        } else {
+            g.drawString("Times up!!", 1100, 500);
+        }
+        
+        timer--; // 14
+        
+        try {
+            Thread.sleep(1000);
+            repaint();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        if (ans_given == 1) {
+            ans_given = 0;
+            timer = 15;
+        } else if (timer < 0) {
+            timer = 15;
+            opt1.setEnabled(true);
+            opt2.setEnabled(true);
+            opt3.setEnabled(true);
+            opt4.setEnabled(true);
+            
+            if (count == 8) {
+                next.setEnabled(false);
+                submit.setEnabled(true);
+            }
+            if (count == 9) { // submit button
+                if (groupoptions.getSelection() == null) {
+                   useranswers[count][0] = "";
+                } else {
+                    useranswers[count][0] = groupoptions.getSelection().getActionCommand();
+                }
+                
+                for (int i = 0; i < useranswers.length; i++) {
+                    if (useranswers[i][0].equals(answers[i][1])) {
+                        score += 10;
+                    } else {
+                        score += 0;
+                    }
+                }
+                setVisible(false);
+                new Score(name, score);
+            } else { // next button
+                if (groupoptions.getSelection() == null) {
+                   useranswers[count][0] = "";
+                } else {
+                    useranswers[count][0] = groupoptions.getSelection().getActionCommand();
+                }
+                count++; // 0 // 1
+                start(count);
+            }
+        }
+        
+    }
+    
+    public void start(int count) {
+        qno.setText("" + (count + 1) + ". ");
+        question.setText(questions[count][0]);
+        opt1.setText(questions[count][1]);
+        opt1.setActionCommand(questions[count][1]);
+        
+        opt2.setText(questions[count][2]);
+        opt2.setActionCommand(questions[count][2]);
+        
+        opt3.setText(questions[count][3]);
+        opt3.setActionCommand(questions[count][3]);
+        
+        opt4.setText(questions[count][4]);
+        opt4.setActionCommand(questions[count][4]);
+        
+        groupoptions.clearSelection();
+    }
+    
+    public static void main(String[] args) {
+        new Quiz("User");
+    }
+}
